cfb1a4775b8db48362ef1aa4f0e188d36bdbfc2d
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-03-28 23:33:34 +0200

    Initial Commit

---
+++ Hrysiuk_Lab5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hrysiuk_Lab5
+//
+//  Created by Анастасія Грисюк on 28.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hrysiuk_Lab5/Hrysiuk_Lab5App.swift
@@ -0,0 +1,17 @@
+//
+//  Hrysiuk_Lab5App.swift
+//  Hrysiuk_Lab5
+//
+//  Created by Анастасія Грисюк on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hrysiuk_Lab5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
